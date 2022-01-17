487b3f01f508fd8f63fe40f07f37beb3757e8a9f
Josac <cristianchoacalo@gmail.com>
2022-01-17 11:08:18 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById("blur_menu").addEventListener("click", hide_menu());
+
+function active_menu(){
+    document.getElementById("responsive_menu").style.right = "0px";
+    document.getElementById("blur_menu").style.display = "block";
+};
+
+
+function hide_menu() {
+    document.getElementById("responsive_menu").style.right = "-220px";
+    document.getElementById("blur_menu").style.display = "none";
+}
