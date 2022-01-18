2750d1c693a534bf3c7249720efc9ad58e5d8946
Cristian Acalo <cristianchoacalo@gmail.com>
2022-01-18 18:12:19 -0500

    header's first version finished

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 function active_menu(){
     document.getElementById("responsive_menu").style.right = "0px";
-    document.getElementById("blur_menu").style.display = "block";
+    document.getElementById("blur_menu").style.left = "0";
+    document.getElementById("blur_menu").style.opacity = "1";
 };
 
 
 function hide_menu() {
     document.getElementById("responsive_menu").style.right = "-220px";
-    document.getElementById("blur_menu").style.display = "none";
+    document.getElementById("blur_menu").style.left = "100vw";
+    document.getElementById("blur_menu").style.opacity = "0";
 }
